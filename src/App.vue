995707f459ea16b14490f44285add53de425e125
vaniafed <ztankist@gmail.com>
2024-03-07 12:35:30 +0300

    feat: heading component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <script>
 import Button from "@/components/Button.vue";
 import Container from "@/components/Container.vue";
+import Heading from "@/components/Heading.vue";
 
 export default {
-  components: { Button, Container },
+  components: { Button, Container, Heading },
 };
 </script>
 
 <template>
-  <Container>
+  <Container :class="[$style.container]">
     hi my name is Button
 
-    <Button text="Подать заявку"></Button>
-    <Button text="Получить консультацию"></Button>
+    <Button>Подать заявку</Button>
+    <Button variant="primary">Получить консультацию</Button>
+
+    <Heading variant="great">H1</Heading>
+    <Heading>H1</Heading>
+    <Heading size="h2" variant="medium_32">H2</Heading>
+    <Heading size="h2" variant="medium_24">H2</Heading>
   </Container>
 </template>
 
-<style></style>
+<style lang="scss" module>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.container * {
+  width: fit-content;
+}
+</style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: "h1",
+    },
+    variant: {
+      type: String,
+      default: "medium_40",
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 v-if="size === 'h1'" :class="[$style[variant]]">
+    <slot></slot>
+  </h1>
+  <h2 v-else-if="size === 'h2'" :class="[$style[variant]]">
+    <slot></slot>
+  </h2>
+  <h3 v-else-if="size === 'h3'" :class="[$style[variant]]">
+    <slot></slot>
+  </h3>
+  <h4 v-else-if="size === 'h4'" :class="[$style[variant]]">
+    <slot></slot>
+  </h4>
+  <h5 v-else-if="size === 'h5'" :class="[$style[variant]]">
+    <slot></slot>
+  </h5>
+  <h6 v-else-if="size === 'h6'" :class="[$style[variant]]">
+    <slot></slot>
+  </h6>
+</template>
+
+<style module lang="scss">
+.great {
+  font-size: 70px;
+  font-weight: 600;
+  line-height: 70px;
+}
+
+.medium_40 {
+  font-size: 40px;
+  font-weight: 500;
+  line-height: 44px;
+}
+
+.medium_32 {
+  font-size: 32px;
+  font-weight: 500;
+  line-height: 38.4px;
+}
+
+.medium_24 {
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 28.8px;
+}
+</style>
