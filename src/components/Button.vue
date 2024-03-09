00985eb0a25b10cc4bc7e985e5af9aa76e0459e3
vaniafed <ztankist@gmail.com>
2024-03-09 09:41:04 +0300

    feat: add button variants

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,22 +1,46 @@
 <script>
 export default {
-  props: { text: Number, variant: String },
-  methods: {
-    log: () => {
-      console.log("hi");
-    },
-  },
+  props: { variant: String },
 };
 </script>
 
 <template>
-  <button @click="log()" class="'button button_' + variant">
-    {{ text }}
+  <button :class="[$style.button, $style.variant]" v-bind="$attrs">
+    <slot></slot>
   </button>
 </template>
 
-<style scoped>
+<style module lang="scss">
 .button {
   padding: 8px 21px;
+  text-align: center;
+  background: $white;
+  color: $black;
+  box-shadow: $shadow;
+  border-radius: 3px;
+  user-select: none;
+  font: 500 16px/24px "TT Firs Neue";
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: $shadow-hover;
+  }
+
+  @include media-tablet {
+    font-size: 14px;
+    line-height: 16.8px;
+  }
+}
+
+.primary {
+  background: $gradient-green;
+
+  &:hover {
+    background: $aqua;
+  }
+
+  &:active {
+    background: $green;
+  }
 }
 </style>
