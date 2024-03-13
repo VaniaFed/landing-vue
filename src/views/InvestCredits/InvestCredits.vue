f08dfccfdf99e013d7fcb31185788e8d7113d86a
VaniaFed <ztankist@gmail.com>
2024-03-13 13:53:54 +0300

    refactor InvestCredits: extract credit subjects into separate component

--- src/views/InvestCredits/InvestCredits.vue
+++ src/views/InvestCredits/InvestCredits.vue
@@ -3,20 +3,18 @@ import BaseHeading from "@/components/BaseHeading.vue";
 import BaseParagraph from "@/components/BaseParagraph.vue";
 import BaseSection from "@/components/BaseSection.vue";
 import BaseStack from "@/components/BaseStack.vue";
-import CircleIcon from "@/components/CircleIcon.vue";
-import IconMspBank from "@/components/icons/IconMspBank.vue";
 import Opportunities from "./Opportunities.vue";
+import CreditSubjects from "./CreditSubjects.vue";
 
 export default {
   props: {},
   components: {
     Opportunities,
+    CreditSubjects,
     BaseSection,
     BaseHeading,
     BaseParagraph,
     BaseStack,
-    CircleIcon,
-    IconMspBank,
   },
   data() {
     return {
@@ -47,49 +45,7 @@ export default {
       Корпорацией МСП и МСП Банк.
     </BaseParagraph>
     <div class="invest-credits__top">
-      <div class="credit-subjects invest-credits__credit-subjects">
-        <div class="credit-subjects__item">
-          <div class="credit-subjects__icons">
-            <CircleIcon class="credit-subjects__icon-button">
-              <IconMspBank class="credit-subjects__icon" />
-            </CircleIcon>
-            <CircleIcon class="credit-subjects__icon-button">
-              <img
-                class="icon-msp-corp"
-                src="/images/msp-corp.png"
-                alt="Корпорация МСП"
-              />
-            </CircleIcon>
-          </div>
-          <div class="credit-subjects__texting">
-            <BaseParagraph weight="500" class="credit-subjects__title"
-              >МСП Банк и&nbsp;Корпорация МСП</BaseParagraph
-            >
-            <BaseParagraph class="credit-subjects__text"
-              >выдают заемные средства</BaseParagraph
-            >
-          </div>
-        </div>
-        <div class="credit-subjects__item">
-          <div class="credit-subjects__icons">
-            <CircleIcon class="credit-subjects__icon-button">
-              <img
-                class="icon-msk-gov"
-                src="/images/moscow-government.png"
-                alt="Правительство Москвы"
-              />
-            </CircleIcon>
-          </div>
-          <div class="credit-subjects__texting">
-            <BaseParagraph weight="500" class="credit-subjects__title"
-              >Правительство Москвы</BaseParagraph
-            >
-            <BaseParagraph class="credit-subjects__text"
-              >субсидирует ставку по кредиту</BaseParagraph
-            >
-          </div>
-        </div>
-      </div>
+      <CreditSubjects class="invest-credits__credit-subjects" />
       <div class="credit-rate invest-credits__credit-rate">
         <BaseHeading class="credit-rate__title" variant="medium_24"
           >Льготная ставка в&nbsp;рамках программы</BaseHeading
@@ -178,106 +134,11 @@ export default {
     flex-grow: 1;
   }
 
-  &__credit-subjects {
+  &:deep(.invest-credits__credit-subjects) {
     margin-bottom: 13px;
 
     @include media-tablet {
       margin-bottom: 0;
-    }
-  }
-}
-
-.credit-subjects {
-  display: flex;
-  gap: 90px;
-
-  @include media-laptop {
-    gap: 30px;
-  }
-
-  @include media-tablet {
-    flex-direction: column;
-    gap: 20px;
-  }
-
-  &__item {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-
-    @include media-tablet {
-      flex-direction: row;
-      align-items: center;
-      gap: 20px;
-    }
-
-    @include media-mobile-large {
-      gap: 16px;
-    }
-  }
-
-  &__icons {
-    display: flex;
-
-    :not(:first-child) {
-      margin-left: -10px;
-
-      @include media-tablet {
-        margin-left: -19px;
-      }
-
-      @include media-mobile-large {
-        margin-left: 0;
-        margin-top: -8px;
-      }
-    }
-
-    @include media-mobile-large {
-      flex-direction: column;
-    }
-  }
-
-  &__icon-button {
-    background: $gray-4;
-
-    @include media-mobile-large {
-      width: 46px;
-      height: 46px;
-    }
-  }
-
-  &__icon {
-    width: 58.2px;
-
-    @include media-mobile-large {
-      width: 30px;
-    }
-  }
-
-  &__title {
-    @include media-tablet {
-      max-width: 189px;
-      line-height: 120%;
-    }
-
-    @include media-mobile-large {
-      font-size: 16px;
-      line-height: 112%;
-    }
-  }
-
-  &__text {
-    margin-top: 10px;
-
-    @include media-tablet {
-      margin-top: 6px;
-      max-width: 170px;
-      line-height: 120%;
-    }
-
-    @include media-mobile-large {
-      max-width: none;
-      line-height: 140%;
     }
   }
 }
@@ -336,22 +197,6 @@ export default {
     @include media-mobile-large {
       font-size: 30px;
     }
-  }
-}
-
-.icon-msp-corp {
-  width: 50px;
-
-  @include media-mobile-large {
-    width: 23px;
-  }
-}
-
-.icon-msk-gov {
-  height: 52px;
-
-  @include media-mobile-large {
-    height: 24px;
   }
 }
 </style>
